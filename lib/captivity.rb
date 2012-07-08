374cebff5dd756c451b0d93349e53965dc75192f
Julik Tarkhanov <me@julik.nl>
2012-07-08 16:45:41 +0200

    Init the thing

---
+++ lib/captivity.rb
@@ -0,0 +1,58 @@
+require 'delegate'
+require 'logger'
+require 'rack/commonlogger'
+
+class Captivity
+  VERSION = '0.0.1'
+  
+  # Rack::CommonLogger is so ingeniously put together that
+  # it does not honor the standard logger levels, it uses #write.
+  # The #write method is not even defined on the standard Logger,
+  # so we flee to a delegate which will convert the logs to
+  # info messages
+  class LoggerWithWriteMethod < SimpleDelegator
+    def write(str)
+      info(str)
+    end
+  end
+  
+  def initialize(app, logger_or_stream = STDERR)
+    @logger = configure_logger_or_stream(logger_or_stream)
+    @app = Rack::CommonLogger.new(app, @logger)
+  end
+    
+  # Calls the contained application. The application will receive
+  # the defined Logger object in the "captivity.logger" of the env hash
+  def call(env)
+    env["captivity.logger"] = @logger
+    @app.call(env)
+  rescue StandardError, LoadError, SyntaxError => exception
+    log_exception(exception)
+    # Reraise to display using Rack
+    raise exception
+  end
+  
+  private
+  
+  def log_exception(exception)
+    @logger.fatal(
+      "\t#{exception.class} (#{exception.message}):\n    " +
+      exception.backtrace.join("\n\t\t") +
+      "\n\n"
+    )
+  end
+  
+  def configure_logger_or_stream(logger_or_stream)
+    if logger_or_stream.respond_to?(:warn) && logger_or_stream.respond_to?(:write)
+      logger_or_stream
+    elsif logger_or_stream.respond_to?(:warn)
+      LoggerWithWriteMethod.new(logger_or_stream)
+    elsif logger_or_stream.respond_to?(:write) # IO-ish
+      logger = Logger.new(logger_or_stream)
+      logger.level = Logger::DEBUG
+      configure_logger_or_stream(logger)
+    elsif logger_or_stream.nil?
+      configure_logger_or_stream(STDERR)
+    end
+  end
+end
--- test/test_captivity.rb
+++ test/test_captivity.rb
@@ -1,7 +1,37 @@
 require 'helper'
+require "stringio"
+
+ENV['RACK_ENV'] = 'test'
 
 class TestCaptivity < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  include Rack::Test::Methods
+  
+  def app
+    @app
   end
+  
+  def test_captivity_logs_request_with_io
+    stream = StringIO.new
+    
+    @app = Rack::Builder.app do
+       use Captivity, stream
+       run lambda { |env| [200, {'Content-Type' => 'text/plain'}, 'OK'] }
+    end
+    
+    get '/boom'
+    assert_match /GET \/boom/, stream.string
+  end
+  
+  def test_captivity_logs_request_with_logger_object
+    stream = StringIO.new
+    
+    @app = Rack::Builder.app do
+       use Captivity, Logger.new(stream)
+       run lambda { |env| [200, {'Content-Type' => 'text/plain'}, 'OK'] }
+    end
+    
+    get '/boom'
+    assert_match /GET \/boom/, stream.string
+  end
+  
 end
